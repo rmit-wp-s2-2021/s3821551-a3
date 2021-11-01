2db5c84f5cf913d59962d5e4bade6c25da832ad9
tomfuren <41816095+tomfuren@users.noreply.github.com>
2021-11-02 04:05:38 +1100

    This is main.php of this assignment

---
+++ team.php
@@ -0,0 +1,76 @@
+<!-- require function -->
+<?php require_once('includes/function.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- require head -->
+        <?php require_once('includes/head.php'); ?>
+    </head>
+    <!-- require header -->
+    <?php require_once('includes/header.php'); ?>
+    <!-- require navigation bar -->
+    <?php require_once('includes/navbar.php'); ?>
+    <!-- body forweb page -->
+    <body>
+        <!-- image with page title -->
+        <div class="wordsonimg filter">
+            <img class="fit-img" src="assets/shaking-hand.jpg" alt="MeetOurTeam">
+            <p>Meet Our Team</p>
+        </div>
+        <!-- make 3 colum -->
+        <div class="members">
+            <!-- 1 colum item -->
+            <div class="member border1">
+                <!-- member picture -->
+                <img src="assets/avataaars 1.png" alt="member1">
+                <!-- name of role -->
+                <center><h3>MARKETING</h3>
+                <!-- description for role -->
+                <p>Marketing is integral in selling products or services in an effective and profitable way, no matter the size of the business. It influences brand and corporate strategy.</p>
+                <!-- SNS icons and link -->
+                <a href="https://twitter.com/home"><i class="fab fa-twitter-square icon-green"></i></a>
+                <a href="https://www.facebook.com/"><i class="fab fa-facebook-square icon-green"></i></a>
+                <a href="https://myaccount.google.com/"><i class="fab fa-google-plus-square icon-green"></i></a>
+                <a href="https://mail.google.com/"><i class="fas fa-envelope-square icon-green"></i></a>
+                <!-- Map images -->
+                <img src="img/mapview.jpg" alt="Map View" />
+            </div>
+
+            <!-- 1 colum item -->
+            <div class="member border2">
+                <!-- member picture -->
+                <img src="assets/avataaars 2.png" alt="member2">
+                <!-- name of role -->
+                <center><h3>DESIGNER</h3>
+                <!-- description for role -->
+                <p>Designers apply creative and open approaches to defining and solving problems, leading to high-quality decisions. This enables businesses and industries to overcome rigid.</p>
+                <!-- SNS icons and link -->
+                <a href="https://twitter.com/home"><i class="fab fa-twitter-square icon-yellow"></i></a>
+                <a href="https://www.facebook.com/"><i class="fab fa-facebook-square icon-yellow"></i></a>
+                <a href="https://myaccount.google.com/"><i class="fab fa-google-plus-square icon-yellow"></i></a>
+                <a href="https://mail.google.com/"><i class="fas fa-envelope-square icon-yellow"></i></a>
+                <!-- Map images -->
+                <img src="img/map2.jpg" alt="Map View" />
+            </div>
+
+            <!-- 1 colum item -->
+            <div class="member border3">
+                <!-- member picture -->
+                <img src="assets/avataaars 3.png" alt="member3">
+                <!-- name of role -->
+                <center><h3>PRODUCTER</h3>
+                <!-- description for role -->
+                <p>A producer is the person responsible for finding and launching a project; arranging financing financing; hiring writers, a director, and key members of the creative team;</p>
+                <!-- SNS icons and link -->
+                <a href="https://twitter.com/home"><i class="fab fa-twitter-square icon-red"></i></a>
+                <a href="https://www.facebook.com/"><i class="fab fa-facebook-square icon-red"></i></a>
+                <a href="https://myaccount.google.com/"><i class="fab fa-google-plus-square icon-red"></i></a>
+                <a href="https://mail.google.com/"><i class="fas fa-envelope-square icon-red"></i></a>
+                <!-- Map images -->
+                <img src="img/map3.jpg" alt="Map View" />
+            </div>
+        </div>
+    </body>
+    <!-- require footer -->
+    <?php require_once('includes/footer.php'); ?>
+</html>
