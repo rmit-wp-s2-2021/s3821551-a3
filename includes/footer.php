ab25c602ce0e82751bd67624c32128ab112b6b34
tomfuren <41816095+tomfuren@users.noreply.github.com>
2021-11-02 04:05:00 +1100

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -2,4 +2,5 @@
 <footer>
     <p><b>last modified time</b>: 2/11/2021 - 3:55</p>
     <p>© All rights reserved by Sakamoto Tomoki - S3821551.</p>
+    <a href="https://github.com/rmit-wp-s2-2021/s3821551-a3">Github Repo</a>
 </footer>
